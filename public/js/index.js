937826f58acad611218ae468bb5721dc1442fabd
Ahmad Hajjar <ash852006@gmail.com>
2019-01-06 14:18:56 +0800

    Added mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -12,16 +12,31 @@ $(document).ready(function () {
 
   socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('MMM Do, YYYY h:mm A');
-    $('#messages').append(`<li> [${formattedTime}] ` + message.from + ' says :' + message.text + '</li>');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+      createdAt: formattedTime,
+      from: message.from,
+      text: message.text
+    });
+    $('#messages').append(html);
   });
 
   socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('MMM Do, YYYY h:mm A');
-    $('#messages').append(`<li> [${formattedTime}] ` + message.from + ' shared their <a href=' + message.url + ' target="new">location</a></li>');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+      createdAt: formattedTime,
+      from: message.from,
+      url: message.url
+    });
+    $('#messages').append(html);
   });
 
   $('#message-form').on('submit', function (e) {
     e.preventDefault();
+    if($('#message').val().trim()===''){
+      return false;
+    }
     socket.emit('createMessage', {
       from: 'a user',
       text: $('#message').val()
@@ -39,12 +54,16 @@ $(document).ready(function () {
       return alert('Sorry. Your browser does not support geolocaiton');
     }
 
+    sendLocationBtn.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+      sendLocationBtn.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         lng: position.coords.longitude,
         lat: position.coords.latitude
       });
     }, function (err) {
+      sendLocationBtn.removeAttr('disabled').text('Send location');
       alert('Unable to fetch your locaiton');
     });
   });
